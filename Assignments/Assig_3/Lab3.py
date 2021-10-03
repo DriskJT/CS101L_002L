0b8d2cdf0241422875b12780516137bb8c6ac100
DriskJT <jeffrey.driskill@gmail.com>
2021-10-02 21:21:52 -0500

    updated files pt 2

--- Assignments/Assig_3/Lab3.py
+++ Assignments/Assig_3/Lab3.py
@@ -4,52 +4,40 @@ CS101L
 Fall21 - M - 7-9:30
 '''
 
-while True:                            
-    print('Please think of a number between and including 1 and 100. \n') 
-    nums = '357'                      
-    result = [ ]                                       
-    for i in nums:                    
-        while True:
-            print('What is the remainder when your number is divided by', i,'? ', end='')
-            f = int(input())
-            if (f >= int(i)):
-                print('The value entered must be less than', i)
-                continue
-            if (f < 0):
-                print('The value entered must be 0 or greater')
-                continue
-            else:
-                if(f != '7'):
-                    print()
-                result.append(f)
-                break                         
-
-
-    for number in range(1,101):
-        if number % 3 == result[0]:
-            if number % 5 == result[1]:
-                if number % 7 == result[2]:
-                    print('Your number was',number)
-                    print('How amazing is that?')
-                    break
-                else:
-                    continue
-            else:
-                continue
-        else:
-            continue                                      
-    
-    while True:
-        answer = ''                 
-        print('Do you want to play again? Y to continue, N to quit ==>')
-        answer = input()
-        if answer == 'Y' or answer =='y' or answer == 'N'or answer == 'n':
-            break
-        else:
-            continue
-
-    if answer == 'N' or answer == 'n':
-        break
-    elif answer == 'Y' or answer == 'n':
-        print()
-        continue
+print('*'*4,'Welcome to the LAB grade calculator!','*'*4)
+name = input('\nWho are we calculating grades for? ==> ')
+lab_grd = int(input('\nEnter the Labs grade? ==> '))
+if lab_grd > 100:
+    lab_grd = 100
+    print('The lab value should have been 100 or less. It has been changed to 100.')
+elif lab_grd < 0:
+    lab_grd = 0
+    print('The lab value should have been zero or greater. It has been changed to zero.')
+exam_grd = int(input('\nEnter the EXAMS grade? ==> '))
+if exam_grd > 100:
+    exam_grd = 100
+    print('The exam value should have been 100 or less. It has been changed to 100.')
+elif exam_grd < 0:
+    exam_grd = 0
+    print('The exam value should have been zero or greater. It has been changed to zero.')
+attend_grd = int(input('\nEnter the Attendance grade? ==> '))
+if attend_grd > 100:
+    attend_grd = 100
+    print('The attendance value should have been 100 or less. It has been changed to 100.')
+elif attend_grd < 0:
+    attend_grd = 0
+    print('The attendance value should have been zero or greater. It has been changed to zero.')
+weight_grd = (exam_grd * .2) + (lab_grd * .7) + (attend_grd * .1)
+if weight_grd >= 90:
+    lett_grd = 'A'
+elif weight_grd >= 80:
+    lett_grd = 'B'
+elif weight_grd >= 70:
+    lett_grd = 'C'
+elif weight_grd >= 60:
+    lett_grd = 'D'
+else:
+    lett_grd = 'F'
+print('\nThe weighted grade for {} is {}'.format(name,weight_grd))
+print('\n{} has a letter grade of {}'.format(name,lett_grd))
+print('\nThanks for using the Lab grade calculator!')
